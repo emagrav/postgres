9d6e15cfad99ee286ed351412604897cb9a14c4f
emagrav <emagrav@gmail.com>
2017-08-03 19:54:11 +0200

    connessione con blocchi try catch finally

--- src/main/java/it/emagrav/postgres/postgres.java
+++ src/main/java/it/emagrav/postgres/postgres.java
@@ -1,24 +1,34 @@
 package it.emagrav.postgres;
- 
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
- 
+
 public class postgres {
- 
-    public static void main(String[] args) throws SQLException {
-         
-        String url = "jdbc:postgresql://localhost:5432/postgres";
-        Properties props = new Properties();
-        props.setProperty("user","test");
-        props.setProperty("password","test");
- 
-        Connection conn = DriverManager.getConnection(url, props);
- 
-        System.out.println("Test di connessione avvenuto con successo");
-         
-        conn.close();
-    }
- 
+
+	public static void main(String[] args) {
+
+		String url = "jdbc:postgresql://localhost:5432/postgres";
+		Properties props = new Properties();
+		props.setProperty("user", "test");
+		props.setProperty("password", "tesat");
+
+		Connection conn = null;
+		try {
+			conn = DriverManager.getConnection(url, props);
+			System.out.println("Test di connessione avvenuto con successo");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
 }
