8043421177863b85307dfbc537dde8deb64de827
emagrav <emagrav@gmail.com>
2017-08-04 11:00:45 +0200

    refactoring

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # postgres
 Progetto di prova per testare DB postgres
+
+# particolarità
+- gestione delle eccezioni: 
+	- utilizzo eccezione personalizzata (classe DBManagerException)
+	- sempre specificata la causa delle'eccezione
+
+- incapsulamento gestione accessi al DB (classe DBManager)
+
+- uso del try-with-resources di Java 8
+
+- uso delle Property
+
+- 
--- src/main/java/it/emagrav/postgres/dao/DBManagerException.java
+++ src/main/java/it/emagrav/postgres/dao/DBManagerException.java
@@ -2,20 +2,22 @@ package it.emagrav.postgres.dao;
 
 public class DBManagerException extends Exception {
 
+	private final static String ERRORE = "Errore durante elaborazione del DBManager";
+	
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 
-	public DBManagerException(Exception e) {
-		this("Errore durante elaborazione del DBManager!", e);
-	}
-
-	public DBManagerException(String string, Exception e) {
-		super(string,e);
-	}
-
 	public DBManagerException(String string) {
-		super (string);
+		super(ERRORE + " : " + string);
+	}
+
+	public DBManagerException(Exception e) {
+		this(ERRORE, e);
+	}
+
+	private DBManagerException(String string, Exception e) {
+		super(string, e);
 	}
 }
