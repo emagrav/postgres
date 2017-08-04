324f347cdb64673a2583659388fbf0fbd532a213
emagrav <emagrav@gmail.com>
2017-08-04 08:21:51 +0200

    Gestione icapsulata degli accessi al DB con DBManager.

--- src/main/java/it/emagrav/postgres/dao/DBManager.java
+++ src/main/java/it/emagrav/postgres/dao/DBManager.java
@@ -4,61 +4,83 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Properties;
 
-public class DBManager implements AutoCloseable{
-	 
-    private static DBManager dbManager;
-    private Properties queryProperties;
-    private Properties dbProperties;
-    private String postgresUrl;
-    private String user;
-    private String password;
- 
-    private DBManager() throws DBManagerException  {
- 
-        try (InputStream dbPropFile = getClass().getResourceAsStream("db.properties");
-             InputStream queryPropFile = getClass().getResourceAsStream("query.properties");) {
-             
-            queryProperties = new Properties();
-            dbProperties = new Properties();
-             
-            dbProperties.load(dbPropFile);
-            queryProperties.load(queryPropFile);
- 
-            postgresUrl = "jdbc:postgresql://" + dbProperties.getProperty("host") + ":"
-                    + dbProperties.getProperty("port") + "/" + dbProperties.getProperty("dbName");
- 
-            user = dbProperties.getProperty("user");
-            password = dbProperties.getProperty("password");
- 
-             
-        } catch (IOException e) {
-            throw new DBManagerException(e);
-        }
-    }
- 
-    public static DBManager getInstance() throws DBManagerException  {
- 
-        if (dbManager == null)
-            dbManager = new DBManager();
- 
-        return dbManager;
-    }
- 
-    public Connection getConnection() throws SQLException{
-        return DriverManager.getConnection(postgresUrl, user, password);
-    }
- 
-    public String getQuery(String queryId) {
-        return queryProperties.getProperty(queryId);
-    }
+public class DBManager {
 
-	@Override
-	public void close() throws Exception {
-		if (dbManager != null)
-			dbManager = null;
+	private static DBManager dbManager;
+	private Properties queryProperties;
+	private Properties dbProperties;
+	private String postgresUrl;
+	private String user;
+	private String password;
+
+	private DBManager() throws DBManagerException {
+
+		try (InputStream dbPropFile = getClass().getResourceAsStream("db.properties");
+				InputStream queryPropFile = getClass().getResourceAsStream("query.properties");) {
+
+			queryProperties = new Properties();
+			dbProperties = new Properties();
+
+			dbProperties.load(dbPropFile);
+			queryProperties.load(queryPropFile);
+
+			postgresUrl = "jdbc:postgresql://" + dbProperties.getProperty("host") + ":"
+					+ dbProperties.getProperty("port") + "/" + dbProperties.getProperty("dbName");
+
+			user = dbProperties.getProperty("user");
+			password = dbProperties.getProperty("password");
+
+		} catch (IOException e) {
+			throw new DBManagerException(e);
+		}
 	}
- 
+
+	public static DBManager getInstance() throws DBManagerException {
+
+		if (dbManager == null)
+			dbManager = new DBManager();
+
+		return dbManager;
+	}
+
+	public Connection getConnection() throws DBManagerException {
+		try {
+			return DriverManager.getConnection(postgresUrl, user, password);
+		} catch (SQLException e) {
+			throw new DBManagerException(e);
+		}
+	}
+
+	public String getQuery(String queryId) {
+		return queryProperties.getProperty(queryId);
+	}
+
+	public void viewTable(Connection cn, String codQuery) throws DBManagerException {
+		try (Statement stm = cn.createStatement();) {
+			String query = this.getQuery(codQuery);
+			
+			if (query==null) {
+				throw new DBManagerException("Codice query " + codQuery + " inesistente.");
+			}
+			
+			ResultSet rs = stm.executeQuery(query);
+			int i = 1;
+			while (rs.next()) {
+				System.out.println("Riga " + i + " : " + rs.getInt(1) + " " + rs.getString(2));
+				i++;
+			}
+		} catch (SQLException e) {
+			throw new DBManagerException(e);
+		}
+	}
+
+	public void viewTable(String codQuery) throws DBManagerException {
+		viewTable(this.getConnection(), codQuery);
+	}
+
 }
--- src/main/java/it/emagrav/postgres/postgres2.java
+++ src/main/java/it/emagrav/postgres/postgres2.java
@@ -1,34 +1,17 @@
 package it.emagrav.postgres;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
-
 import it.emagrav.postgres.dao.DBManager;
 import it.emagrav.postgres.dao.DBManagerException;
 
 public class postgres2 {
 
 	public static void main(String[] args) {
-		
-		try (DBManager dbm = DBManager.getInstance();){
-		
-			Connection cn = dbm.getConnection();
-			
-			Statement stm = cn.createStatement();
-			ResultSet rs = stm.executeQuery(dbm.getQuery("query1"));
-			
-			int i=1;
-			while (rs.next()) {
-				System.out.println("Riga " + i + " : " + rs.getInt(1) + " " + rs.getString(2));
-				i++;
-			}
-		} catch (DBManagerException e) {
-			e.printStackTrace();
-		} catch (Exception e1) {
+
+		try {
+			DBManager dbm = DBManager.getInstance();
+			dbm.viewTable("query1");
+		} catch (DBManagerException e1) {
 			e1.printStackTrace();
 		}
-		
 	}
-
 }
