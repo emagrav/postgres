70c9c1297a608b2d12f11eadf4c8c0086e268f00
emagrav <emagrav@gmail.com>
2017-08-04 08:20:25 +0200

    Migliorata gestione delle eccezioni

--- src/main/java/it/emagrav/postgres/dao/DBManagerException.java
+++ src/main/java/it/emagrav/postgres/dao/DBManagerException.java
@@ -1,7 +1,5 @@
 package it.emagrav.postgres.dao;
 
-import java.sql.SQLException;
-
 public class DBManagerException extends Exception {
 
 	/**
@@ -10,7 +8,14 @@ public class DBManagerException extends Exception {
 	private static final long serialVersionUID = 1L;
 
 	public DBManagerException(Exception e) {
-		// TODO Auto-generated constructor stub
+		this("Errore durante elaborazione del DBManager!", e);
 	}
 
+	public DBManagerException(String string, Exception e) {
+		super(string,e);
+	}
+
+	public DBManagerException(String string) {
+		super (string);
+	}
 }
